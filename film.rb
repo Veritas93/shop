88bf5b288810299da542465b2b68ce675ae39cda
Sergey Kostin <63309253+Veritas93@users.noreply.github.com>
2022-12-19 09:33:38 +0200

    Add new method for film class

--- film.rb
+++ film.rb
@@ -1,5 +1,8 @@
 
 # Создадим класс Film наследуемый от Product
 class Film < Product
-  # напишем позже заготовка
+  # метод который возвращает - название, год и режисера
+  def to_s(names)
+    return "Фильм '#{names.name}', #{names.age}, #{names.avtor}, #{names.price} руб. (осталось #{names.count}) "
+  end
 end
