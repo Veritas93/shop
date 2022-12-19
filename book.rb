86a21ad47d5844ab377741e0567f0e3614c6bee3
Sergey Kostin <63309253+Veritas93@users.noreply.github.com>
2022-12-19 09:32:58 +0200

    Add new method for book class

--- book.rb
+++ book.rb
@@ -1,4 +1,7 @@
 # Создадим класс Book наследуемый от Product
 class Book < Product
-  # напишем позже загатовка
+  # метод который возвращает - название, жанр и автора
+  def to_s(names)
+    return "Книга '#{names.name}', #{names.zhanr}, #{names.avtor}, #{names.price} руб. (осталось #{names.count}) "
+  end
 end
