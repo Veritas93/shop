98d493812abce161b17533e71050fa0edc4e13a3
Sergey Kostin <63309253+Veritas93@users.noreply.github.com>
2022-12-19 09:32:10 +0200

    Add some producs

--- main.rb
+++ main.rb
@@ -2,6 +2,14 @@
 require_relarive 'product'
 require_relarive 'book'
 require_relarive 'film'
+
 # Передаем имя объекта, цену и количество на складе в hash при создании экземпляра класса
-leon = Film.new(name: "Леон", price: 990, count: 5)
-puts "Фильм #{leon.name} стоит #{leon.price} руб."
+leon = Film.new(name: "Леон", age: 1994, avtor: "реж. Люк Бессон", price: 990, count: 5)
+stupid = Film.new(name: "Дурак", age: 2014, avtor: "реж. Юрий Быков", price: 390, count: 1)
+idiot = Book.new(name: "Идиот", zhanr: "роман",  avtor: "Федор Достоевский", price: 1500, count: 10)
+prestuplenie = Book.new(name: "Преступление и наказание", zhanr: "роман",  avtor: "Федор Достоевский", price: 1700, count: 8)
+
+sklad = [leon, stupid, idiot, prestuplenie]
+for i in sklad
+  puts i.to_s(i)
+end
