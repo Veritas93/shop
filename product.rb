81698e0a0e641e18b374df86c0f19cdd884f8497
Sergey Kostin <63309253+Veritas93@users.noreply.github.com>
2022-12-19 09:35:06 +0200

    Add new fields

--- product.rb
+++ product.rb
@@ -1,12 +1,13 @@
-# Создадим класс Product
-# Класс имеет три поля которые будут записываться во врямя создания экземплярам name(название товара), price(цена) и count(количестов на складе)
 class Product
-  attr_reader :name, :price, :count
+  attr_reader :name, :price, :count, :zhanr, :avtor, :age
   # В конструктор класса подаем параметр params
   # В качестве params будет будет использоваться Hash
   def initialize(params)
     @name = params[:name]
     @price = params[:price]
     @count = params[:count]
+    @zhanr = params[:zhanr]
+    @avtor = params[:avtor]
+    @age = params[:age]
   end
 end
